9dd0648e792c8ad445b2b2803b4eb318e2c4d977
Mohammed Salama <mohammed.salama@ihorizons.com>
2020-09-05 12:48:18 +0300

    add retrofit di

---
+++ app/src/main/java/com/tiendito/BMisrMoviesApp.kt
@@ -0,0 +1,8 @@
+package com.tiendito
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BMisrMoviesApp: Application() {
+}
---
+++ app/src/main/java/com/tiendito/di/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.tiendito.di
+
+
+import com.tiendito.api.MoviesApis
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideFlickerService(): MoviesApis {
+
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MoviesApis::class.java)
+    }
+
+}
